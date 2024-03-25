61859a63af6015f6798db43b4d8931df26bed63d
Kavindu404 <piyumal.kavindu99@icloud.com>
2024-03-25 10:52:42 -0400

    updated get_dataset to accept multiple years

--- rcan/utils.py
+++ rcan/utils.py
@@ -6,16 +6,18 @@ import glob
 import os   
 from resize_transforms import ResizeTensor
 
-def get_dataset(pth):
+def get_dataset(pths):
     data_tensors = []
-    files = glob.glob(os.path.join(pth, '*2d.nc'))
-    for file in files[:10]:
-        data = xr.open_dataset(file)
-        if 'ssh' in data.data_vars:
-            data.ssh.values[torch.isnan(torch.tensor(data.ssh.values))]=0
-            data.u_barotropic_velocity.values[torch.isnan(torch.tensor(data.u_barotropic_velocity.values))]=0
-            data.v_barotropic_velocity.values[torch.isnan(torch.tensor(data.v_barotropic_velocity.values))]=0
-            data_tensors.append(torch.stack((torch.tensor(data.ssh.values).squeeze(), torch.tensor(data.u_barotropic_velocity.values).squeeze(), torch.tensor(data.v_barotropic_velocity.values).squeeze()), dim=0))
+
+    for pth in pths:
+        files = glob.glob(os.path.join(pth, '*2d.nc'))
+        for file in files[:10]:
+            data = xr.open_dataset(file)
+            if ('ssh' in data.data_vars) and ('u_barotropic_velocity' in data.data_vars) and ('v_barotropic_velocity' in data.data_vars):
+                data.ssh.values[torch.isnan(torch.tensor(data.ssh.values))]=0
+                data.u_barotropic_velocity.values[torch.isnan(torch.tensor(data.u_barotropic_velocity.values))]=0
+                data.v_barotropic_velocity.values[torch.isnan(torch.tensor(data.v_barotropic_velocity.values))]=0
+                data_tensors.append(torch.stack((torch.tensor(data.ssh.values).squeeze(), torch.tensor(data.u_barotropic_velocity.values).squeeze(), torch.tensor(data.v_barotropic_velocity.values).squeeze()), dim=0))
         
     return torch.stack([t for t in data_tensors], dim=0)
 
