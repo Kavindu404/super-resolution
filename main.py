9fcf53c8be5e58983ddc147206a6b2e438752205
Kavindu404 <piyumal.kavindu99@icloud.com>
2024-03-29 02:28:41 -0400

    model initialization and data loading

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import json
 import argparse
 from rcan.utils import get_data
 from torch.utils.tensorboard import SummaryWriter
+from rcan.model import UNetSR
+from rcan.resize_transforms import ResizeTensor
+from torch.utils.data import DataLoader
+from torch.nn.parallel import DataParallel
+from rcan.dataset import SRDataset
+from rcan.loss import nll_loss
 
 
 if __name__ == "__main__":
@@ -24,11 +30,16 @@ if __name__ == "__main__":
     lr_path = "/unity/f1/BOEM_GOMb0.04/data/"
     hr_path = "/unity/g2/BOEM_GOMb0.01/data/"
 
-    train_pths = [os.join(lr_path, str(year)) for year in train_years]
-    val_pths = [os.join(hr_path, str(val_year))]
+    train_data_pths = [os.join(lr_path, str(year)) for year in train_years]
+    train_lbl_pths = [os.join(hr_path, str(year)) for year in train_years]
+    val_data_pths = [os.join(lr_path, str(val_year))]
+    val_lbl_pths = [os.join(hr_path, str(val_year))]
 
-    train_data = get_data(train_pths)
-    val_data = get_data(val_pths)
+    train_data = ResizeTensor((256, 256))(get_data(train_data_pths))
+    val_data = ResizeTensor((256, 256))(get_data(val_data_pths))
+
+    train_lbl = ResizeTensor((1024, 1024))(get_data(train_lbl_pths))
+    val_lbl = ResizeTensor((1024, 1024))(get_data(val_lbl_pths))
     
     data_root = '../../../../data/'
     model_dp = 'models/'
@@ -46,6 +57,7 @@ if __name__ == "__main__":
     input_dim = meta["parameters"]["input_dim"]
     output_dim = meta["parameters"]["output_dim"]
     scaling_factor = meta["parameters"]["scale"]
+    batch_size = meta["parameters"]["batch_size"]
     
 
     model_dir = model_dp + str(meta_version)
@@ -65,6 +77,23 @@ if __name__ == "__main__":
     
     writer = SummaryWriter(log_dir=logs_dir)
 
+    model = UNetSR(in_channels=input_dim, out_channels=output_dim, num_filters=64, upscale_factor=scaling_factor)
+
+    if len(gpus) > 1:
+        print(f"Using {len(gpus)} GPUs for training.")
+        model = DataParallel(model)
+    
+    train_ds = SRDataset(train_data, train_lbl)
+    val_ds = SRDataset(val_data, val_lbl)
+
+    train_dl = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
+    val_dl = DataLoader(val_ds, batch_size=batch_size, shuffle=False)
+
+
+    criterion = nll_loss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+
+    model.to(device)
     
 
 
